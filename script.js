77b93edcb56e3ea551e5e45df66acc188647e523
Unknown1407 <118428191+Unknown1407@users.noreply.github.com>
2023-08-26 11:28:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const deletetext = document.getElementById("colorSelect");
+let colors = []
+for(let i=0;i<deletetext.length;i++)
+	{
+		colors[i] = deletetext[i];
+	}
+function deletion() {
+	deletetext.classList.remove(colors);	
+}
+deletion();
